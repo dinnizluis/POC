b4c82d6609d4f3bad5406121a964ccfcd12fc3f7
Luis  Diniz <dinnizluis@Luiss-MacBook-Pro.local>
2018-03-19 16:19:53 -0300

    KMeans alterado para trabalhar apenar com categoria probe e registros regulares

--- Implementação/kmeans.py
+++ Implementação/kmeans.py
@@ -2,21 +2,18 @@ import pandas as pd
 from sklearn import cluster
 
 #Loading the dataset
-df = pd.read_csv("20Percent_5_labels.csv", sep='\t') 
+df = pd.read_csv("KDDTest+_probe_no_label.csv", sep='\t') 
 #df = pd.read_csv("KDDTrain+_20Percent_pcd_no_label.csv", sep='\t') 
-y = pd.read_csv("KDDTrain+_20Percent_pcd.csv", sep='\t')
+y = pd.read_csv("KDDTest+_probe.csv", sep='\t')
 
-print(df.shape)
-k_means = cluster.KMeans(n_clusters = 5)
+k_means = cluster.KMeans(n_clusters = 2)
 k_means.fit(df)
 
 n_rows, n_cols = df.shape
-print(k_means.labels_)
 correct = 0
-#for i in range(0, n_rows):
-#	if(y['label'][i] == k_means.labels_[i]):
-#		correct += 1
+for i in range(0, n_rows):
+	if(y['label'][i] == k_means.labels_[i]):
+		correct += 1
 
-#print(correct)
-#rate = (correct/n_rows)*100
-#print("rate = ", rate)
+rate = (correct/n_rows)*100
+print("rate = ", rate)
