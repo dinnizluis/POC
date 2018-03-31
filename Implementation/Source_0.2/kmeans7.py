7b1655c48343a870fc8a1e531bf4f71de2dc544b
Luis  Diniz <dinnizluis@Luiss-MacBook-Pro.local>
2018-03-31 14:34:37 -0300

    Scripts able to perform kmeans on 3 different kinds of input

---
+++ Implementation/Source_0.2/kmeans7.py
@@ -0,0 +1,22 @@
+#Script 6 - Luis Diniz - 31/03/2018
+
+#Goals: - Run KMeans with 7 features indicated [see reference]
+#       - Export results to csv file
+
+#Step 1 :: Import libraries
+from sklearn import cluster
+import pandas as pd 
+
+#Step 2 :: Load data
+path_in = '/Users/dinnizluis/Dropbox/Computer Science/00_20132018_Atividades Extracurriculares/Iniciação Científica/Análise de Dados/POC/Implementation/Input/'
+df = pd.read_csv(path_in+'nslkdd.csv', sep='\t')
+
+#Step 3 :: Execute KMeans
+k_means = cluster.KMeans(n_clusters = 2)
+k_means.fit(df)
+
+#Step 4 :: Export results
+path_out = '/Users/dinnizluis/Dropbox/Computer Science/00_20132018_Atividades Extracurriculares/Iniciação Científica/Análise de Dados/POC/Implementation/Output/'
+res = pd.DataFrame(data = k_means.labels_)
+res.to_csv(path_out+'kmeans7f.csv', sep='\t', index = False)
+print('Data successfully exported!')
